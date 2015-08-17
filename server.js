e89066c1890cdca5ec80458612211c9ad8defe68
hman1 <hman@walmartlabs.com>
2015-08-16 23:11:29 -0700

    initilize the app, finish login window

--- server.js
+++ server.js
@@ -2,39 +2,124 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
+    fs = require('fs'),
     users = [];
 //specify the html we will use
-app.use('/', express.static(__dirname + '/www'));
-//bind the server to the 80 port
-//server.listen(3000);//for local test
-server.listen(process.env.PORT || 3000);//publish to heroku
-//server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);//publish to openshift
-//console.log('server started on port'+process.env.PORT || 3000);
-//handle the socket
-io.sockets.on('connection', function(socket) {
-    //new user login
-    socket.on('login', function(nickname) {
-        if (users.indexOf(nickname) > -1) {
-            socket.emit('nickExisted');
-        } else {
-            socket.userIndex = users.length;
-            socket.nickname = nickname;
-            users.push(nickname);
-            socket.emit('loginSuccess');
-            io.sockets.emit('system', nickname, users.length, 'login');
-        };
+app.use('/', express.static(__dirname + '/src'));
+app.get('/comments.json', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/comments.json', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(comments);
     });
-    //user leaves
-    socket.on('disconnect', function() {
-        users.splice(socket.userIndex, 1);
-        socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
+  });
+});
+
+server.listen(process.env.PORT || 3000);
+// Keep track of which names are used so that there are no duplicates
+var userNames = (function () {
+  var names = {};
+
+  var claim = function (name) {
+    if (!name || names[name]) {
+      return false;
+    } else {
+      names[name] = true;
+      return true;
+    }
+  };
+
+  // find the lowest unused "guest" name and claim it
+  var getGuestName = function () {
+    var name,
+      nextUserId = 1;
+
+    do {
+      name = 'Guest ' + nextUserId;
+      nextUserId += 1;
+    } while (!claim(name));
+
+    return name;
+  };
+
+  // serialize claimed names as an array
+  var get = function () {
+    var res = [];
+    for (user in names) {
+      res.push(user);
+    }
+
+    return res;
+  };
+
+  var free = function (name) {
+    if (names[name]) {
+      delete names[name];
+    }
+  };
+
+  return {
+    claim: claim,
+    free: free,
+    get: get,
+    getGuestName: getGuestName
+  };
+}());
+io.sockets.on('connection', function(socket){
+  var name = userNames.getGuestName();
+
+  // send the new user their name and a list of users
+  socket.emit('init', {
+    name: name,
+    users: userNames.get()
+  });
+
+  // notify other clients that a new user has joined
+  socket.broadcast.emit('user:join', {
+    name: name
+  });
+
+  // broadcast a user's message to other users
+  socket.on('send:message', function (data) {
+    socket.broadcast.emit('send:message', {
+      user: name,
+      text: data.text
     });
-    //new message get
-    socket.on('postMsg', function(msg, color) {
-        socket.broadcast.emit('newMsg', socket.nickname, msg, color);
+  });
+
+  // validate a user's name change, and broadcast it on success
+  socket.on('change:name', function (data, fn) {
+    if (userNames.claim(data.name)) {
+      var oldName = name;
+      userNames.free(oldName);
+
+      name = data.name;
+
+      socket.broadcast.emit('change:name', {
+        oldName: oldName,
+        newName: name
+      });
+
+      fn(true);
+    } else {
+      fn(false);
+    }
+  });
+
+  // clean up when a user leaves, and broadcast it to other users
+  socket.on('disconnect', function () {
+    socket.broadcast.emit('user:left', {
+      name: name
     });
-    //new image get
-    socket.on('img', function(imgData, color) {
-        socket.broadcast.emit('newImg', socket.nickname, imgData, color);
-    });
+    userNames.free(name);
+  });
 });
---
+++ src/scripts/wechat.js
@@ -0,0 +1,231 @@
+/**
+ * @jsx React.DOM
+ */
+var socket = io.connect();
+
+var Users = [];
+var Messages = [];
+
+var UsersList = React.createClass({
+	render: function(){
+		var renderUser = function(user){
+			return <li> { user} </li>
+		};
+		return (
+			<div class='users'>
+				<h3> Online Users </h3>
+				<ul>{ this.props.users.map(renderUser)} </ul>
+			</div>
+		);
+	}
+});
+
+var Message = React.createClass({
+	render: function(){
+		return(
+			<div class="message">
+				<strong>{this.props.user}</strong> :
+				{this.props.text}
+			</div>
+		)
+	}
+});
+
+var MessageList = React.createClass({
+	render: function(){
+		var renderMessage = function(message){
+			return <Message user={message.user} text={message.text} />
+		}
+		return (
+			<div class='messages'>
+				<h2> Conversation: </h2>
+				{ this.props.messages.map(renderMessage)}
+			</div>
+		);
+	}
+});
+
+var MessageForm = React.createClass({
+
+	getInitialState: function(){
+		return {text: ''};
+	},
+
+	handleSubmit : function(e){
+		e.preventDefault();
+		var message = {
+			user : this.props.user,
+			text : this.state.text
+		}
+		this.props.onMessageSubmit(message);
+		this.setState({ text: '' });
+	},
+
+	changeHandler : function(e){
+		this.setState({ text : e.target.value });
+	},
+
+	render: function(){
+		return(
+			<div class='message_form'>
+				<h3>Write New Message</h3>
+				<form onSubmit={this.handleSubmit}>
+					<input onChange={this.changeHandler} value={this.state.text} />
+				</form>
+			</div>
+		);
+	}
+});
+
+var ChangeNameForm = React.createClass({
+	getInitialState: function(){
+		return {newName: ''};
+	},
+
+	onKey : function(e){
+		this.setState({ newName : e.target.value });
+	},
+
+	handleSubmit : function(e){
+		e.preventDefault();
+		var newName = this.state.newName;
+		this.props.onChangeName(newName);
+		this.setState({ newName: '' });
+	},
+
+	render: function(){
+		return(
+			<div class='change_name_form'>
+				<h3> Change Name </h3>
+				<form onSubmit={this.handleSubmit}>
+					<input onChange={this.onKey} value={this.state.newName} />
+				</form>
+			</div>
+		);
+	}
+});
+
+var ChatApp = React.createClass({
+
+	getInitialState: function(){
+
+		socket.on('init', this.initialize);
+		socket.on('send:message', this.messageRecieve);
+		socket.on('user:join', this.userJoined);
+		socket.on('user:left', this.userLeft);
+		socket.on('change:name', this.userChangedName);
+
+		return {users: [], messages:[], text: ''};
+	},
+
+	initialize: function(data){
+		this.setState({ users: data.users, user: data.name});
+	},
+
+	messageRecieve: function(message){
+		this.state.messages.push(message);
+		this.setState();
+	},
+
+	userJoined: function(data){
+		this.state.users.push(data.name);
+		this.state.messages.push({
+			user: 'APLICATION BOT',
+			text : data.name +' Joined'
+		});
+		this.setState();
+	},
+
+	userLeft: function(data){
+		var index = this.state.users.indexOf(data.name);
+		this.state.users.splice(index, 1);
+		this.state.messages.push({
+			user: 'APLICATION BOT',
+			text : data.name +' Left'
+		});
+		this.setState();
+
+	},
+
+	userChangedName : function(data){
+		var oldName = data.oldName;
+		var newName = data.newName;
+		this.state.users.splice(this.state.users.indexOf(oldName), 1, newName);
+		this.state.messages.push({
+			user: 'APLICATION BOT',
+			text : 'Change Name : ' + oldName + ' ==> '+ newName
+		});
+		this.setState();
+
+	},
+
+	handleMessageSubmit : function(message){
+		this.state.messages.push(message);
+		this.setState();
+
+		socket.emit('send:message', message);
+	},
+
+	handleChangeName : function(newName){
+		var that = this;
+		var oldName = this.state.user;
+		socket.emit('change:name', { name : newName}, function(result){
+			if(!result){
+				alert('There was an error changing your name');
+			}else{
+				var index = that.state.users.indexOf(oldName);
+				that.state.users.splice(index, 1, newName);
+				that.setState();
+			}
+		});
+	},
+
+	render : function(){
+		return (
+			<div>
+				<UsersList users={this.state.users} />
+				<MessageList messages={this.state.messages} />
+				<MessageForm onMessageSubmit={this.handleMessageSubmit} user={this.state.user} />
+				<ChangeNameForm onChangeName={this.handleChangeName} />
+			</div>
+		);
+	}
+});
+var LoginForm = React.createClass({
+	getInitialState: function(){
+    return {style: {
+			opacity: '0'
+		}}
+  },
+	componentDidMount: function() {
+		 React.findDOMNode(this.refs.login_input).focus();
+ 	},
+	handleChange : function(){
+    this.setState({
+      style: {
+				opcacity: '1'
+			}
+    });
+	},
+  render : function(){
+    return (
+      <div className={'introForm'}>
+        <div>
+          <div className={'greeting'}>
+						<i className={"fa fa-commenting-o"}></i>
+            <div className={"inline"}>What’s your name?</div>
+          </div>
+          <div className={'input'}>
+						<i className={"fa fa-commenting-o"}></i>
+            <div className={'input'}>
+              <input className={"inline"} type="text" ref="login_input" onChange={this.handleChange}/>
+            </div>
+            <button type="button" style={this.state.style}><span>Submit</span></button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+})
+React.render(<ChatApp/>, $('#chat-window')[0]);
+React.render(<LoginForm/>, $('#login-window')[0]);
